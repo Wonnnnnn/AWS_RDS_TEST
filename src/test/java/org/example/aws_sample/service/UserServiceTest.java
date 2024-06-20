5c2fe47c375047f1f827414b61228b6e44ecdd6f
01022549653@naver.com <>
2024-06-20 15:49:54 +0900

    user test

---
+++ src/test/java/org/example/aws_sample/service/UserServiceTest.java
@@ -0,0 +1,26 @@
+package org.example.aws_sample.service;
+
+import org.example.aws_sample.dto.User.UserCreateDto;
+import org.example.aws_sample.dto.User.UserResponseDto;
+import org.example.aws_sample.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class UserServiceTest {
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void addUser() {
+        UserCreateDto userCreateDto = new UserCreateDto("aaa","apw","namea","a@email.com");
+        UserResponseDto dto= userService.addUser(userCreateDto);
+        assertThat(dto.getUsername()).isEqualTo(userCreateDto.getUsername());
+    }
+}
