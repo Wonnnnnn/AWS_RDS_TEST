8c2c0bde22d5cb060e5a50cb953f34dba0fe9234
01022549653@naver.com <>
2024-06-21 11:41:58 +0900

    aws sample classes

--- src/test/java/org/example/aws_sample/AwsSampleApplicationTests.java
+++ src/test/java/org/example/aws_sample/AwsSampleApplicationTests.java
@@ -3,7 +3,7 @@ package org.example.aws_sample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AwsSampleApplicationTests.class)
 class AwsSampleApplicationTests {
 
     @Test
--- src/test/java/org/example/aws_sample/service/UserServiceTest.java
+++ src/test/java/org/example/aws_sample/service/UserServiceTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
